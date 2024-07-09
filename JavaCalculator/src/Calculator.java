2bd064e59af2bd89e496eccef86a9e6fb4e20ff7
TboyBell <bellnewtech@gmail.com>
2024-07-09 14:07:20 +0100

    Second Commit

--- JavaCalculator/src/Calculator.java
+++ JavaCalculator/src/Calculator.java
@@ -1,14 +1,17 @@
+public class Calculator {
 
-
-public class Calculator{
-
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		
-		//Class BackEnd For Objects
+			
+		//Runs Project From This Class,
+		// Makes COde Faster and Cleaner.
 		Backend calc = new Backend();
+		
+		
+		
 	}
 	
 
